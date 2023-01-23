c1a7225bd1e7fbc31b4a735684d1521d1822b3b5
tom26th <thomas.shallcross@outlook.com>
2023-01-23 18:50:06 +0000

    added plot_dat function

--- DBC_plots.R
+++ DBC_plots.R
@@ -1,10 +1,29 @@
+source("colourscale.R")
 
 plot_dp <- function(dens, dis){
 	plot(log(dis), log(dens), pch = 19, cex = 0.5)
 
-	logdist<-seq(log(dis)[1],range(dis,finite=T)[2],length.out=100);
-	logdist<-seq(-10,20)
+	logdist<-seq(from = min(log(dis)), to = range(dis,finite=T)[2], length.out=100);
 
-	lines(logdist,-2*logdist ,lty=2,col="black")
-	
+	lines(logdist,-2*logdist ,lty=2,col="black")	
+}
+
+plot_dat <- function(dat, cl = NULL, outliers = NULL){
+
+	if(is.null(cl)){
+		cols <- "black"
+	}else{
+		n_cl <- max(cl)
+		cols <- rainbow(n_cl)[cl]
+	}
+
+	if(is.null(outliers)){
+		thresh <- rep(T, nrow(dat))
+	}else{
+		thresh <- outliers
+	}
+
+
+	plot(dat, pch = 1, col = cols, ann = F, xaxt = "n", yaxt = "n", cex = 0.5)
+	points(dat[thresh,], pch = 19, col = cols[thresh], cex = 0.5)
 }
