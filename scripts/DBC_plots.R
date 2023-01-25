5936554331d52bf3ce56982ec3ab10942598ea0a
Tom Shallcross (home) <thomas.shallcross@outlook.com>
2023-01-25 13:09:26 +0000

    updated plot_dat function

--- scripts/DBC_plots.R
+++ scripts/DBC_plots.R
@@ -1,29 +1,41 @@
-source("colourscale.R")
 
-plot_dp <- function(dens, dis, threshold = 0, Dim = 2){
-	plot(log(dis), log(dens), pch = 19, cex = 0.5)
+plot_dp <- function(dens, dis, threshold = NULL, Dim = 2){
 
-	logdist<-seq(from = min(log(dis)), to = range(dis,finite=T)[2], length.out=100);
+	plot(log(dis)
+	     ,log(dens)
+	     ,pch = 19
+	     ,cex = 0.5
+	     ,xlab = "log(distance to nearest neighbour of higher density)"
+	     ,ylab = "log(local density)"
+	)
 
-	lines(logdist,-Dim * logdist + threshold,lty=2,col="black")	
+	logdist<-seq(from = min(log(dis)), to = range(dis,finite = T)[2], length.out = 100);
+
+	lines(logdist, -Dim * logdist + threshold, lty = 2, col = "black")	
 }
 
-plot_dat <- function(dat, cl = NULL, outliers = NULL){
+plot_dat <- function(dat, cl = NULL, outliers = NULL, centers = NULL){
 
-	if(is.null(cl)){
-		cols <- "black"
-	}else{
+	n_dat <- nrow(dat)
+	cols <- rep("grey", n_dat)
+
+	if(!is.null(cl)){
 		n_cl <- max(cl)
 		cols <- viridis(n = n_cl)[cl]
 	}
 
 	if(is.null(outliers)){
-		thresh <- rep(T, nrow(dat))
+		thresh <- rep(T, n_dat)
 	}else{
 		thresh <- outliers
 	}
-
-
+	
 	plot(dat, pch = 1, col = cols, ann = F, xaxt = "n", yaxt = "n", cex = 0.5)
 	points(dat[thresh,], pch = 19, col = cols[thresh], cex = 0.5)
+
+	if(!is.null(centers)){
+		n_centers <- length(centers)	
+		cols <- viridis(n = n_centers)
+		points(dat[centers,], pch = 19, col = cols, cex = 1)
+	}
 }
